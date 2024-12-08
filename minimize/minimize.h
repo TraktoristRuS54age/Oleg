541c21792c1385abcf583a7b54aa9db6f8f2d824
Danchik <124835518+TraktoristRuS54age@users.noreply.github.com>
2024-12-08 22:05:43 +0300

    Create minimize.h

---
+++ minimize/minimize.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <iostream>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
+#include <queue>
